1f0494f46132f3078637d8876b9a9a3c5ca314af
sandeer25 <dassanayakesaneth@gmail.com>
2026-02-01 23:36:13 +0530

    fix(constants): correct RANDOM_RESPONSE_CHANCE value from 0.8 to 0.2

--- src/config/constants.js
+++ src/config/constants.js
@@ -4,7 +4,7 @@ module.exports = {
   MAX_STORED_MESSAGES: 20,
 
   // Response behavior
-  RANDOM_RESPONSE_CHANCE: 0.8,
+  RANDOM_RESPONSE_CHANCE: 0.2,
 
   // Typing delays (in ms)
   MIN_TYPING_DELAY: 1000,
